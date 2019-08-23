ec99515b4b8de2757439281cfd791c72d6fedbf0
Zagorouiko <Zagorouiko@gmail.com>
2019-08-22 19:47:37 -0700

    Added event callbacks for the pressure plates

--- Source/PuzzlePlatforms/PlatformTrigger.h
+++ Source/PuzzlePlatforms/PlatformTrigger.h
@@ -27,4 +27,11 @@ private:
 	UPROPERTY(VisibleAnywhere)
 		class UBoxComponent* TriggerVolume;
 
+	//Dynamic event u function
+	UFUNCTION()
+	void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
 };
